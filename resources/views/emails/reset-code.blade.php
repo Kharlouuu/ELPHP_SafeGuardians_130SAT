c700d227261529c271f2282cd93d82dd416ff56e
Kharlouuu <karlharlou132@gmail.com>
2025-05-08 02:03:27 +0800

    Fix on Forgot Pass

---
+++ resources/views/emails/reset-code.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Password Reset Code</title>
+</head>
+<body>
+    <p>Hello,</p>
+    <p>You requested a password reset. Your reset code is:</p>
+    <h2>{{ $code }}</h2>
+    <p>Enter this code in the app to reset your password.</p>
+    <p>If you did not request this, please ignore this email.</p>
+    <p>Thanks,<br>Coin Crate Team</p>
+</body>
+</html>
